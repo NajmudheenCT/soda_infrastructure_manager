f927ee58c902deab9116025d7ad29f3e4cb397cd
Namudheen CT <najmudheen.c.t@huawei.com>
2020-04-07 17:32:02 +0530

    Initial project skeleton : Directory structure

---
+++ docs/README.rst
@@ -0,0 +1,5 @@
+
+Welcome to sample's documentation!
+==================================
+
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+
+
+from setuptools import setup, find_packages
+from os import path
+
+
+
+
+setup(
+    name="infra_structure_manager",
+    version=1.0,
+
+    description="An infrastructure management project for  SODA Foundation",
+
+
+
+
+    # author and license
+    author="SODA Foundation",
+    author_email="opensds-tech-discuss@lists.opensds.io",
+
+
+
+)
